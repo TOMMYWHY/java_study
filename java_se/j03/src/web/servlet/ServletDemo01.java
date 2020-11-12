dc830ce05c9aedce7fa561a90da3e21c9150141b
tommy <tommywhy1989@gmail.com>
2020-11-12 15:42:27 +0800

    sevlet

---
+++ java_se/j03/src/web/servlet/ServletDemo01.java
@@ -0,0 +1,2 @@
+package web.servlet;public class ServletDemo01 {
+}
---
+++ java_se/j04/src/servlet/Demo01.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+
+public class Demo01 implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("ri le gou");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
