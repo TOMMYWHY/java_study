88525281411c18bda1c608f20f5ba8ed85c9294f
tommy <tommywhy1989@gmail.com>
2020-12-10 23:16:07 +0800

    spring annotation 01

---
+++ java_se/j09_framework/spring_study/demo2_xml_di/src/main/java/com/tommy/why/ba03/Student.java
@@ -0,0 +1,32 @@
+package com.tommy.why.ba02;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+        System.out.println("none param constructor~!");
+    }
+
+    public Student(String myname, int myage) {
+        System.out.println("param constructor~!");
+        this.name = myname;
+        this.age = myage;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ java_se/j09_framework/spring_study/demo3_annotation/src/main/java/com/tommy/why/ba01/Student.java
@@ -0,0 +1,2 @@
+package com.tommy.why.ba01;public class Student {
+}
