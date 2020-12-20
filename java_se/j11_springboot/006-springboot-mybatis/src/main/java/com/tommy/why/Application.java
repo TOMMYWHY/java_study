d6a2ce8083898fcb04c56271389ae817f3e24d39
tommy <tommywhy1989@gmail.com>
2020-12-20 11:40:47 +0800

    springboot:mybatis done

--- java_se/j11_springboot/006-springboot-mybatis/src/main/java/com/tommy/why/Application.java
+++ java_se/j11_springboot/006-springboot-mybatis/src/main/java/com/tommy/why/Application.java
@@ -1,9 +1,11 @@
 package com.tommy.why;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.tommy.why.dao")
 public class Application {
 
 	public static void main(String[] args) {
