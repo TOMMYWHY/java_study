aa12d9b075a46fcea53911e3c1c4a017a322ea71
tommy <tommywhy1989@gmail.com>
2020-12-29 22:43:12 +0800

    spring review

--- java_se/j13_spring/springDemo01/src/main/java/com/tommy/why/service/UserService.java
+++ java_se/j13_spring/springDemo01/src/main/java/com/tommy/why/service/UserService.java
@@ -1,7 +1,18 @@
 package com.tommy.why.service;
 
+import com.tommy.why.dao.UserDao;
+
 public class UserService {
+    private  UserDao userDao = null;
+
+    public void setUserDaoo(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     public void test(){
         System.out.println("UserService....");
+        userDao.test();
+
+
     }
 }
--- java_se/j13_spring/springDemo01/src/test/java/com/tommy/why/AppTest.java
+++ java_se/j13_spring/springDemo01/src/test/java/com/tommy/why/AppTest.java
@@ -6,6 +6,7 @@ import com.tommy.why.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * Unit test for simple App.
@@ -17,12 +18,20 @@ public class AppTest
     {
         String springConfig = "spring.xml";
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext(springConfig);
+
+//        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("/Users/Tommy/Desktop/studyInMassey/Java_study/java_se/j13_spring/springDemo01/src/main/resources/spring.xml");
+
         UserService userService =(UserService) applicationContext.getBean("userService");
-        userService.test();
+//        userService.test();
     }
 
     @Test
     public void test02(){
+        String springConfig = "spring02.xml";
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(springConfig);
+
+        UserService userService =(UserService) applicationContext.getBean("userService");
+        userService.test();
 
     }
 }
