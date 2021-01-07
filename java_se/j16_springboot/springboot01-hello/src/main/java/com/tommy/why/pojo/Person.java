d64b2f30f65061ccd5ef8743d86def347d01e39e
tommy <tommywhy1989@gmail.com>
2021-01-07 15:18:23 +0800

    springboot config

---
+++ java_se/j16_springboot/springboot01-hello/src/main/java/com/tommy/why/pojo/Person.java
@@ -0,0 +1,104 @@
+package com.tommy.why.pojo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+//    @Value("tommy")
+    private String name;
+    private Integer age;
+    private Boolean happy;
+    private Date birth;
+    private Map<String, Object> map;
+    private List<Object> list;
+    private Dog dog;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", happy=" + happy +
+                ", birth=" + birth +
+                ", map=" + map +
+                ", list=" + list +
+                ", dog=" + dog +
+                '}';
+    }
+
+    public Person() {
+    }
+
+    public Person(String name, Integer age, Boolean happy, Date birth, Map<String, Object> map, List<Object> list, Dog dog) {
+        this.name = name;
+        this.age = age;
+        this.happy = happy;
+        this.birth = birth;
+        this.map = map;
+        this.list = list;
+        this.dog = dog;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Boolean getHappy() {
+        return happy;
+    }
+
+    public void setHappy(Boolean happy) {
+        this.happy = happy;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Object> map) {
+        this.map = map;
+    }
+
+    public List<Object> getList() {
+        return list;
+    }
+
+    public void setList(List<Object> list) {
+        this.list = list;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+}
--- java_se/j16_springboot/springboot01-hello/src/test/java/com/tommy/why/Springboot01HelloWorldApplicationTests.java
+++ java_se/j16_springboot/springboot01-hello/src/test/java/com/tommy/why/Springboot01HelloWorldApplicationTests.java
@@ -1,13 +1,19 @@
 package com.tommy.why;
 
+import com.tommy.why.pojo.Dog;
+import com.tommy.why.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot01HelloWorldApplicationTests {
 
+	@Autowired
+	private Person person;
 	@Test
 	void contextLoads() {
+		System.out.println(person);
 	}
 
 }
