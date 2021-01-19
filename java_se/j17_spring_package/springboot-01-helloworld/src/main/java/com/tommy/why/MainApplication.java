91657346f1e0f71d0a3c75a189af645601057de7
tommy <tommywhy1989@gmail.com>
2021-01-19 23:11:59 +0800

    springboot review

--- java_se/j17_spring_package/springboot-01-helloworld/src/main/java/com/tommy/why/MainApplication.java
+++ java_se/j17_spring_package/springboot-01-helloworld/src/main/java/com/tommy/why/MainApplication.java
@@ -1,2 +1,11 @@
-package com.tommy.why;public class MainApplication {
+package com.tommy.why;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
 }
--- java_se/j17_spring_package/springboot-01-helloworld/src/main/java/com/tommy/why/controller/HelloController.java
+++ java_se/j17_spring_package/springboot-01-helloworld/src/main/java/com/tommy/why/controller/HelloController.java
@@ -1,2 +1,18 @@
-package com.tommy.why.controller;public class HelloController {
+package com.tommy.why.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@ResponseBody
+//@RestController
+public class HelloController {
+
+//    @ResponseBody
+    @RequestMapping("/hello")
+    public String handle01(){
+        return "hello Tommy";
+    }
 }
