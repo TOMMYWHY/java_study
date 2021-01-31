3ed2e15e9cdfdb66d690c4adc0104a1db501c935
tommy <tommywhy1989@gmail.com>
2021-01-31 11:04:41 +0800

    j19

--- java_se/j19_data_structure/structures/src/main/java/arrays/FindRepeatNumber.java
+++ java_se/j19_data_structure/structures/src/main/java/arrays/FindRepeatNumber.java
@@ -2,9 +2,7 @@ package arrays;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /*
 * https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
@@ -49,9 +47,48 @@ public class FindRepeatNumber {
         return -1;
     }
 
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
+        }
+        System.out.println(set);
+
+        for(int i = 1 ; i <= nums.length; i++){
+            if(!set.contains(i)){
+                result.add(i);
+            }
+        }
+        return result;
+    }
+    public List<Integer> findDisappearedNumbers1(int[] nums) {
+         List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = Math.abs(nums[i]);
+            int index = num -1;
+            if(nums[index] > 0){
+                nums[index] = - nums[index];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if( nums[i]>0){
+                result.add(i+1);
+            }
+
+        }
+        return result;
+    }
 
     @Test
-    public void test(){
+    public void test_findDisappearedNumbers(){
+        int [] nums = {4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers1(nums));
+    }
+
+    @Test
+    public void test_findRepeatNumber(){
         int [] nums = {2, 3, 1, 0, 2, 5, 3};
         System.out.println(findRepeatNumber(nums));
     }
